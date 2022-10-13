168d2b15ba4bad230fbbc0e125ea320ef54dae3d
flamel666 <marcus.kaio3@gmail.com>
2022-10-13 17:42:20 +0200

    server.js

--- server.js
+++ server.js
@@ -5,10 +5,13 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('../../dist/java-galaxy/'));
+//app.use(express.static('../../dist/java-galaxy/'));
+
+app.use(express.static(__dirname + "/docs"));
 
 app.get('/*', (req, res) => {
-    res.sendFile('index.html', { root: '../../dist/java-galaxy/' });
+   // res.sendFile('index.html', { root: '../../dist/java-galaxy/' });
+   res.sendFile( path.resolve( __dirname, 'docs/index.html' ) );
   });
 
 // Start the app by listening on the default Heroku port
